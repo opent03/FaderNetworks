76620fe8ca234d6c1784608392d7e4eb00f70ee2
root <root@instance-1.us-east1-b.c.handwriting-229719.internal>
2019-04-04 15:56:39 +0000

    Add parallelization of preprocessing pipeline

--- data/preprocess.py
+++ data/preprocess.py
@@ -4,23 +4,41 @@ import matplotlib.image as mpimg
 import cv2
 import numpy as np
 import torch
+from joblib import Parallel, delayed
 
 
-N_IMAGES = 100000
-IMG_SIZE = 128
+N_IMAGES =  120000 
+IMG_SIZE = 256 
 IMG_PATH = 'images_%i_%i.pth' % (IMG_SIZE, IMG_SIZE)
 ATTR_PATH = 'attributes.pth'
-
+all_images = []
 
 def preprocess_images():
 
     if os.path.isfile(IMG_PATH):
         print("%s exists, nothing to do." % IMG_PATH)
         return
-
+    global all_images
     print("Reading images from img_align_celeba/ ...")
     #raw_images = []
-    all_images = []
+    
+    def single_processing(i): 
+        if i % 10000 == 0:
+            print(i)
+        image = mpimg.imread('img_align_celeba/%06i.jpg' % i)[20:-20]
+
+        # Image processing
+        assert image.shape == (178, 178, 3)
+        if IMG_SIZE < 178:
+            image = cv2.resize(image, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_AREA)
+        elif IMG_SIZE > 178:
+            image = cv2.resize(image, (IMG_SIZE, IMG_SIZE), interpolation=cv2.INTER_LANCZOS4)
+        assert image.shape == (IMG_SIZE, IMG_SIZE, 3)
+        global all_images
+        return image 
+        
+    all_images = Parallel(n_jobs=8)(delayed(single_processing)(i) for i in range(1, N_IMAGES + 1))
+    '''
     for i in range(1, N_IMAGES + 1):
         if i % 10000 == 0:
             print(i)
@@ -36,7 +54,7 @@ def preprocess_images():
         all_images.append(image)
 
         #raw_images.append(mpimg.imread('img_align_celeba/%06i.jpg' % i)[20:-20])
-
+    '''
     #if len(raw_images) != N_IMAGES:
         #raise Exception("Found %i images. Expected %i" % (len(raw_images), N_IMAGES))
     if len(all_images) != N_IMAGES:
@@ -55,14 +73,22 @@ def preprocess_images():
         assert image.shape == (IMG_SIZE, IMG_SIZE, 3)
         all_images.append(image)
     '''
+    '''
     data = np.concatenate([img.transpose((2, 0, 1))[None] for img in all_images], 0)
     data = torch.from_numpy(data)
     assert data.size() == (N_IMAGES, 3, IMG_SIZE, IMG_SIZE)
 
     print("Saving images to %s ..." % IMG_PATH)
     torch.save(data[:20000].clone(), 'images_%i_%i_20000.pth' % (IMG_SIZE, IMG_SIZE))
-    torch.save(data, IMG_PATH)
+    torch.save(data, IMG_PATH)'''
+    all_images = np.concatenate([img.transpose((2, 0, 1))[None] for img in all_images], 0)
+    all_images = torch.from_numpy(all_images)
+    assert all_images.size() == (N_IMAGES, 3, IMG_SIZE, IMG_SIZE)
 
+    print("Saving images to %s ..." % IMG_PATH)
+    torch.save(all_images[:20000].clone(), 'images_%i_%i_20000.pth' % (IMG_SIZE, IMG_SIZE))
+    torch.save(all_images, IMG_PATH)
+ 
 
 def preprocess_attributes():
 
